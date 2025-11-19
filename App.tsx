0a3bb2d036fe7b3748a010c4b2e10ed0b81bc2b4
Sushanth <sushanth.2805@gmail.com>
2025-11-19 23:34:50 +0530

    Refactor imports in App.tsx to default imports

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { About } from './components/About';
-import { Work } from './components/Work';
-import { Services } from './components/Services';
-import { Team } from './components/Team';
-import { Contact } from './components/Contact';
+import  Navbar  from './components/Navbar';
+import  Hero  from './components/Hero';
+import  About  from './components/About';
+import  Work  from './components/Work';
+import  Services  from './components/Services';
+import Team  from './components/Team';
+import  Contact  from './components/Contact';
 
 const App: React.FC = () => {
   return (
